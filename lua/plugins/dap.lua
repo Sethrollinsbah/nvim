c0ea3e7b26fde844c31a8765bd00d5b1bc3bf3c8
seth <sethryanrollins@gmail.com>
2025-05-21 12:16:10 -0400

    fixed nvim dap adapter

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,22 +1,30 @@
-local dap = require "dap"
-
-dap.adapters.codelldb = {
-  type = "server",
-  port = "${port}",
-  executable = {
-    command = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/adapter/codelldb",
-    args = { "--port", "${port}" },
-  },
-}
-
-dap.configurations.rust = {
+---@type LazySpec[]
+return {
   {
-    name = "Launch",
-    type = "codelldb",
-    request = "launch",
-    program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file") end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {},
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/adapter/codelldb",
+          args = { "--port", "${port}" },
+        },
+      }
+
+      dap.configurations.rust = {
+        {
+          name = "Launch",
+          type = "codelldb",
+          request = "launch",
+          program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file") end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+        },
+      }
+    end,
   },
 }
